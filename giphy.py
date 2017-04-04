518b8d66491a1a08edc629fcdc06ce7673e409c6
Michiel Gardner <michiel@gardner.nu>
2017-04-04 19:39:02 +0200

    choose tag

--- giphy.py
+++ giphy.py
@@ -3,11 +3,16 @@ import requests
 from pprint import pprint
 import time
 import random
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--tag", required=True)
+args = vars(parser.parse_args())
 
 api_key = 'dc6zaTOxFJmzC' # GitHub API key
 
 while True:
-  gif = requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat')
+  gif = requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=%s' % args['tag'])
 
   with open('giphy%s.gif' % random.randint(1, 3), 'wb') as f:
     f.write(requests.get(gif.json()['data']['image_original_url']).content)
