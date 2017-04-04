7a74b8351f6621973b639b2067fc416f26fbd0eb
Michiel Gardner <michiel@gardner.nu>
2017-04-04 19:45:42 +0200

    'basic' exception handling

--- giphy.py
+++ giphy.py
@@ -12,10 +12,13 @@ args = vars(parser.parse_args())
 api_key = 'dc6zaTOxFJmzC' # GitHub API key
 
 while True:
-  gif = requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=%s' % args['tag'])
+  try:
+    gif = requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=%s' % args['tag'])
 
-  with open('giphy%s.gif' % random.randint(1, 3), 'wb') as f:
-    f.write(requests.get(gif.json()['data']['image_original_url']).content)
-    print 'downloaded', gif.json()['data']['image_original_url']
+    with open('giphy%s.gif' % random.randint(1, 3), 'wb') as f:
+      f.write(requests.get(gif.json()['data']['image_original_url']).content)
+      print 'downloaded', gif.json()['data']['image_original_url']
+  except:
+    time.sleep(10)
 
-  time.sleep(1)
+  time.sleep(10)
