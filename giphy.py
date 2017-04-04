4fa2175f7d46462de0b79b5c4411ce9824a5e9ae
Michiel Gardner <michiel@gardner.nu>
2017-04-04 19:00:11 +0200

    first

---
+++ giphy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import requests
+from pprint import pprint
+import time
+import random
+
+api_key = 'dc6zaTOxFJmzC'
+
+while True:
+  gif = requests.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat')
+
+  with open('giphy%s.gif' % random.randint(1, 3), 'wb') as f:
+    f.write(requests.get(gif.json()['data']['image_original_url']).content)
+    print 'downloaded', gif.json()['data']['image_original_url']
+
+  time.sleep(1)
